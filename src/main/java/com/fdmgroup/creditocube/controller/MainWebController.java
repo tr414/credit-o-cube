54942064bd163e7d30f3d747ff94cd616e49250d
Morella Paciencia <morella.paciencia@fdmgroup.com>
2024-04-15 14:47:07 +0800

    Created customer controller class

--- src/main/java/com/fdmgroup/creditocube/controller/MainWebController.java
+++ src/main/java/com/fdmgroup/creditocube/controller/MainWebController.java
@@ -36,21 +36,7 @@ public class MainWebController {
 	
 	@Autowired
 	CustomerService customerService;
-	
-	
-//	// check if they can login as a user, not just a customer
-//	private boolean checkValidUsernameAndPassword(String username, String password) {
-//		// check if username exists in database
-//		
-//		// if username exists
-//		if (!customerService.findCustomerByUsername(username).isEmpty()) {
-//			User user = customerService.findCustomerByUsername(username).get(0);
-//			if (user.getPassword().equals(password)){
-//				return true;
-//			}
-//		}
-//		return false;
-//	}
+
 	
 	@GetMapping("/") // http://localhost:8080/
 	public String slashIndex() {
@@ -58,54 +44,9 @@ public class MainWebController {
 		// return("something");
 	}
 	
-	// login as a customer
-	@GetMapping("/login") // http://localhost:8080/login
-	public String slashLogin() {
-		return ("login");
-	}
+
 	
-//	// login as a customer
-//    @PostMapping("/login")
-//    public String processLogin(@RequestParam("username") String username, 
-//                               @RequestParam("password") String password, 
-//                               HttpSession session, Model model) {
-//        // if username exists and password corresponds to correct password
-//        if (checkValidUsernameAndPassword(username, password)) {
-//        	
-//            // Authentication successful; set user session
-//        	System.out.println("Authentication was successful!");
-//            session.setAttribute("currentUser", username);
-//            return "customerDashboard";
-//        } else {
-//            // Authentication failed
-//        	model.addAttribute("loginFailed", true);
-//            return "login";
-//        }
-//    }
-    
-    // register as a customer 
-	@GetMapping("/register") // http://localhost:8080/login
-	public String slashRegister() {
-		return ("register");
-	}
-	
-	// registering as a customer
-//    @PostMapping("/login")
-//    public String processCustomerRegistration(
-//    		@RequestParam("username") String username, 
-//            @RequestParam("password") String password, 
-//            @RequestParam("firstName") String firstName,
-//            @RequestParam("lastName") String lastName,
-//            @RequestParam("nric") String nric,
-//            @RequestParam("salary") double salary,
-//            HttpSession session, 
-//            Model model) {
-//    	
-//    	// idk if spring security will check if this username and passwords are valid??
-//    	
-//    	// assuming it does
-//    	
-//    }
+
 	
 	
     
