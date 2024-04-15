c4413f28f47d2e8f05c46e5897c34bc03195b288
Timothy Chai <timothy.chai@fdmgroup.com>
2024-04-15 09:45:09 +0800

    Initial commit

---
+++ src/main/java/com/fdmgroup/creditocube/CreditCardAppApplication.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.creditocube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fdmgroup/creditocube/CreditCardAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.creditocube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
