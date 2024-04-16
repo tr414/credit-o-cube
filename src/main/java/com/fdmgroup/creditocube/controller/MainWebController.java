1c14d8593b6c8207a22275f7da74ad86804cbd36
Timothy Chai <timothy.chai@fdmgroup.com>
2024-04-16 14:49:49 +0800

    Minor commits to web controller and application.properties

--- src/main/java/com/fdmgroup/creditocube/controller/MainWebController.java
+++ src/main/java/com/fdmgroup/creditocube/controller/MainWebController.java
@@ -1,22 +1,12 @@
 package com.fdmgroup.creditocube.controller;
 
-import java.util.ArrayList;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.fdmgroup.creditocube.model.Customer;
-import com.fdmgroup.creditocube.model.User;
 
 import com.fdmgroup.creditocube.service.CustomerService;
 import com.fdmgroup.creditocube.service.DebitAccountService;
 import com.fdmgroup.creditocube.service.UserService;
-
-import jakarta.servlet.http.HttpSession;
 
 /*
  * This controller handles requests for 
@@ -27,30 +17,20 @@ import jakarta.servlet.http.HttpSession;
  */
 @Controller
 public class MainWebController {
-	
+
 	@Autowired
 	UserService userService;
-	
+
 	@Autowired
 	DebitAccountService debitAccountService;
-	
+
 	@Autowired
 	CustomerService customerService;
 
-	
 	@GetMapping("/") // http://localhost:8080/
 	public String slashIndex() {
 		return ("index");
 		// return("something");
 	}
-	
-
-	
-
-	
-	
-    
-
-	
 
 }
