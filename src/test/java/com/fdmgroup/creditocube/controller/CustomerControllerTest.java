0f61946a77ec71fff5af8f6ddddc7d52a56f6702
Tanay Rishi <Tanay.Rishi@fdmgroup.com>
2024-04-17 09:34:39 +0800

    Set up test configuration for customer controller

--- src/test/java/com/fdmgroup/creditocube/controller/CustomerControllerTest.java
+++ src/test/java/com/fdmgroup/creditocube/controller/CustomerControllerTest.java
@@ -3,21 +3,28 @@ package com.fdmgroup.creditocube.controller;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import com.fdmgroup.creditocube.service.CustomerService;
 import com.fdmgroup.creditocube.service.DebitAccountService;
 import com.fdmgroup.creditocube.service.UserService;
 
-@WebMvcTest(CustomerController.class)
+@SpringBootTest
 public class CustomerControllerTest {
 
-    @Autowired
+	@Autowired
+    private WebApplicationContext context;
+	
     private MockMvc mockMvc;
 
     // Mock the services used in CustomerController
@@ -29,6 +36,14 @@ public class CustomerControllerTest {
 
     @MockBean
     private CustomerService customerService;
+    
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
 
     @Test
     public void testSlashLogin_ShouldReturnLoginView() throws Exception {
