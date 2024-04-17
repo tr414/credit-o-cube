44d5ad1c36332c0fe15262398ed184d0a7d2f2c1
Morella Paciencia <morella.paciencia@fdmgroup.com>
2024-04-17 10:01:35 +0800

    Javascript for submitting change details form

--- src/test/java/com/fdmgroup/creditocube/controller/CustomerControllerTest.java
+++ src/test/java/com/fdmgroup/creditocube/controller/CustomerControllerTest.java
@@ -6,34 +6,34 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
 
 import com.fdmgroup.creditocube.service.CustomerService;
 import com.fdmgroup.creditocube.service.DebitAccountService;
 import com.fdmgroup.creditocube.service.UserService;
 
-@WebMvcTest(CustomerController.class)
+@SpringBootTest
 public class CustomerControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+	@Autowired
+	private WebApplicationContext context;
+	private MockMvc mockMvc;
 
-    // Mock the services used in CustomerController
-    @MockBean
-    private UserService userService;
+	// Mock the services used in CustomerController
+	@MockBean
+	private UserService userService;
 
-    @MockBean
-    private DebitAccountService debitAccountService;
+	@MockBean
+	private DebitAccountService debitAccountService;
 
-    @MockBean
-    private CustomerService customerService;
+	@MockBean
+	private CustomerService customerService;
 
-    @Test
-    public void testSlashLogin_ShouldReturnLoginView() throws Exception {
-        mockMvc.perform(get("/login"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("login"));
-    }
+	@Test
+	public void testSlashLogin_ShouldReturnLoginView() throws Exception {
+		mockMvc.perform(get("/login")).andExpect(status().isOk()).andExpect(view().name("login"));
+	}
 }
