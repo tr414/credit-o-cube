7f62692c9133674199e1ed60c9eb71428b7320f1
Tanay Rishi <Tanay.Rishi@fdmgroup.com>
2024-04-18 17:40:44 +0800

    New Credit Card transaction page added

--- src/main/java/com/fdmgroup/creditocube/controller/CreditCardTransactionController.java
+++ src/main/java/com/fdmgroup/creditocube/controller/CreditCardTransactionController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import com.fdmgroup.creditocube.model.CreditCard;
 import com.fdmgroup.creditocube.model.CreditCardTransaction;
+import com.fdmgroup.creditocube.service.CreditCardService;
 import com.fdmgroup.creditocube.service.CreditCardTransactionService;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -20,15 +21,15 @@ public class CreditCardTransactionController {
 	@Autowired
 	CreditCardTransactionService transactionService;
 	
-//	@Autowired
-//	CreditCardService cardService;
+	@Autowired
+	CreditCardService cardService;
 	
 	@PostMapping("/card-transactions")
 	public String getAllCardTransactions(Model model, HttpServletRequest request) {
 		BigDecimal cardId = new BigDecimal(request.getParameter("cardId"));
 		
-		//CreditCard card = cardService.findCreditCardById(cardId.longValue()).orElse(null);
-		CreditCard card = new CreditCard();
+		CreditCard card = cardService.findCardByCardId(cardId.longValue()).orElse(null);
+		// CreditCard card = new CreditCard();
 		
 		List<CreditCardTransaction> cardTransactions = transactionService.findAllCreditCardTransactions(card);
 		model.addAttribute("transactions", cardTransactions);
--- src/main/java/com/fdmgroup/creditocube/service/CreditCardTransactionService.java
+++ src/main/java/com/fdmgroup/creditocube/service/CreditCardTransactionService.java
@@ -1,20 +1,91 @@
 package com.fdmgroup.creditocube.service;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Optional;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 
 import com.fdmgroup.creditocube.model.CreditCard;
 import com.fdmgroup.creditocube.model.CreditCardTransaction;
+import com.fdmgroup.creditocube.model.Merchant;
 import com.fdmgroup.creditocube.repository.CreditCardTransactionRepository;
+
+import jakarta.annotation.PostConstruct;
 
 @Service
 public class CreditCardTransactionService {
 	
+	private static final Logger logger = LogManager.getLogger(CreditCardTransactionService.class);
+	
 	@Autowired
 	private CreditCardTransactionRepository repo;
+	
+//	@Autowired
+//	private ResourceLoader resourceLoader;
+//	
+//	
+//	/**
+//	 * Initializes the merchant database with data from a CSV file. This method is
+//	 * automatically invoked after the bean's properties have been set, ensuring
+//	 * that the database is populated at application startup.
+//	 */
+//	@PostConstruct
+//	public void loadDataOnStartup() {
+//		logger.info("Starting database initialization from CSV file");
+//		importCSV("classpath:data/card-transactions.csv");
+//	}
+//
+//	/**
+//	 * Imports merchant data from a CSV file specified by the resource path.
+//	 * 
+//	 * Opens a CSV file, reads its contents, and either updates existing merchants
+//	 * or adds new ones. Each record in the CSV file is expected to contain a
+//	 * merchant code, category, and active status.
+//	 *
+//	 * @param resourcePath A String representing the path to the CSV file in the
+//	 *                     classpath.
+//	 * @throws Exception if there are issues opening or reading the file or parsing
+//	 *                   the CSV.
+//	 */
+//	public void importCSV(String resourcePath) {
+//		Resource resource = resourceLoader.getResource(resourcePath);
+//		try (BufferedReader reader = new BufferedReader(
+//				new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8))) {
+//			CSVFormat csvFormat = CSVFormat.DEFAULT.builder().setHeader().setSkipHeaderRecord(true).build();
+//			try (CSVParser csvParser = new CSVParser(reader, csvFormat)) {
+//				for (CSVRecord record : csvParser) {
+//					BigDecimal transactionId = new BigDecimal(record.get("Transaction Id"));
+//					Optional<CreditCardTransaction> existingTransaction = repo.findById(transactionId.longValue());
+//
+//					CreditCardTransaction transaction = existingTransaction.orElse(new CreditCardTransaction());
+//					transaction.setTransactionCard(null);
+//					transaction.setMerchantCode(null);
+//					transaction.setTransactionAmount(0);
+//					transaction.setTransactionDate(null);
+//					//transaction.setActive(Boolean.parseBoolean(record.get("Active")));
+//					repo.save(transaction);
+//
+//					logger.debug("Saved or updated transaction with id {}", transactionId);
+//				}
+//			}
+//			logger.info("Successfully loaded transaction data from CSV.");
+//		} catch (Exception e) {
+//			logger.error("Failed to load transaction data from CSV", e);
+//		}
+//	}
+	
 	
 	public Optional<CreditCardTransaction> createCreditCardTransaction(CreditCardTransaction transaction) {
 		Optional<CreditCardTransaction> createdTransaction;
