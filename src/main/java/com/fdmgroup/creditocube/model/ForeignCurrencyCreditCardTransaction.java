e10b1010d7fedf5268e0de4cccc29ff9cee1ce09
Tanay Rishi <Tanay.Rishi@fdmgroup.com>
2024-04-18 15:38:29 +0800

    Add CredtCardTransaction service and repo files. Add child class to represent foreign currency transactions

---
+++ src/main/java/com/fdmgroup/creditocube/model/ForeignCurrencyCreditCardTransaction.java
@@ -0,0 +1,47 @@
+package com.fdmgroup.creditocube.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class ForeignCurrencyCreditCardTransaction extends CreditCardTransaction {
+	private String transactionCurrency;
+	private double exchangeRate;
+	
+
+	public ForeignCurrencyCreditCardTransaction() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ForeignCurrencyCreditCardTransaction(CreditCard transactionCard, Merchant merchantCode,
+			double cashbackAmount, LocalDateTime transactionDate, double transactionAmount, String transactionCurrency, double exchangeRate) {
+		super(transactionCard, merchantCode, cashbackAmount, transactionDate, transactionAmount);
+		this.transactionCurrency = transactionCurrency;
+		this.exchangeRate = exchangeRate;
+	}
+
+	public ForeignCurrencyCreditCardTransaction(String transactionCurrency, double exchangeRate) {
+		super();
+		this.transactionCurrency = transactionCurrency;
+		this.exchangeRate = exchangeRate;
+	}
+
+	public String getTransactionCurrency() {
+		return transactionCurrency;
+	}
+
+	public void setTransactionCurrency(String transactionCurrency) {
+		this.transactionCurrency = transactionCurrency;
+	}
+
+	public double getExchangeRate() {
+		return exchangeRate;
+	}
+
+	public void setExchangeRate(double exchangeRate) {
+		this.exchangeRate = exchangeRate;
+	}
+	
+	
+}
