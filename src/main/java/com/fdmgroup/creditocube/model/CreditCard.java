da2ebcc7b732dc6cfc63f9a826feb152cbd391c8
Tanay Rishi <Tanay.Rishi@fdmgroup.com>
2024-04-18 13:03:32 +0800

    Add Entity annotation and auto gen ID fields to credit card and merchant classes to allow the app to start up

--- src/main/java/com/fdmgroup/creditocube/model/CreditCard.java
+++ src/main/java/com/fdmgroup/creditocube/model/CreditCard.java
@@ -1,5 +1,14 @@
 package com.fdmgroup.creditocube.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class CreditCard {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long cardId;
 
 }
--- src/main/java/com/fdmgroup/creditocube/model/Merchant.java
+++ src/main/java/com/fdmgroup/creditocube/model/Merchant.java
@@ -1,5 +1,13 @@
 package com.fdmgroup.creditocube.model;
 
-public class Merchant {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
+public class Merchant {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long merchantId;
 }
