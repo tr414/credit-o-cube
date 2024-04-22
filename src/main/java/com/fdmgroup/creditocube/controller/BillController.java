c1cbafec3835c3e6668e09c37133b0c445f92aa0
Tanay Rishi <Tanay.Rishi@fdmgroup.com>
2024-04-22 14:08:51 +0800

    Add method to calculate and add interest fee to any outstanding payment at the end of the billing cycle.

--- src/main/java/com/fdmgroup/creditocube/controller/BillController.java
+++ src/main/java/com/fdmgroup/creditocube/controller/BillController.java
@@ -21,41 +21,5 @@ public class BillController {
 	
 	@Autowired
 	CreditCardService cardService;
-	
-	
-	@PostMapping("/pay-outstanding-balance")
-	public String payOutstandingBalance(Model model, HttpServletRequest request) {
-		BigDecimal cardId = new BigDecimal(request.getParameter("cardId"));
 
-//		CreditCard card = cardService.findCardByCardId(cardId.longValue()).orElse(null);
-		// amended code above to return optional rather than null - tim
-		Optional<CreditCard> optionalCard = cardService.findCardByCardId(cardId.longValue());
-		if (optionalCard.isEmpty()) {
-			return "redirect:/creditcard-dashboard";
-		}
-		CreditCard card = optionalCard.get();
-
-		List<CreditCardTransaction> cardTransactions = transactionService.findAllCreditCardTransactions(card);
-		model.addAttribute("transactions", cardTransactions);
-		
-		return ("card-transactions");
-	}
-	
-	@PostMapping("/pay-minimum-balance")
-	public String payMinimumBalance(Model model, HttpServletRequest request) {
-		BigDecimal cardId = new BigDecimal(request.getParameter("cardId"));
-
-//		CreditCard card = cardService.findCardByCardId(cardId.longValue()).orElse(null);
-		// amended code above to return optional rather than null - tim
-		Optional<CreditCard> optionalCard = cardService.findCardByCardId(cardId.longValue());
-		if (optionalCard.isEmpty()) {
-			return "redirect:/creditcard-dashboard";
-		}
-		CreditCard card = optionalCard.get();
-
-		List<CreditCardTransaction> cardTransactions = transactionService.findAllCreditCardTransactions(card);
-		model.addAttribute("transactions", cardTransactions);
-		
-		return ("card-transactions");
-	}
 }
